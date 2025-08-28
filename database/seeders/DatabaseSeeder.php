f16e90bc08277684c56087a56232a3aa4b756500
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-28 10:34:07 +0530

    Add BackfillCategorySeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
- 
- 
-        $this->call([SubCategorySeeder::class,AdminUserSeeder::class,CategorySeeder::class,]);
-       
+
+
+        $this->call([SubCategorySeeder::class,AdminUserSeeder::class,CategorySeeder::class,BackfillCategorySeeder::class]);
+
     }
 }
 
